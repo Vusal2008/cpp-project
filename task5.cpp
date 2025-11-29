a4f6ae4f854a3de68535746ccb8328670b8e086d
Vusal2008 <velekberov80@gmail.com>
2025-11-30 00:44:05 +0400

    level 5
    Refactor edediOrta function to print the average directly.

--- task5.cpp
+++ task5.cpp
@@ -1,32 +1,16 @@
-# include <iostream>
-
+#include <iostream>
 using namespace std;
 
-// EDEDLERIN EDEDIORTASINI TAPAN FUNCTION//
-double edediOrta(double a,double b,double c){
-	
-	int ed=(a+b+c)/3;
-	
-	return ed;
-	
+void edediOrta(double a, double b, double c)
+{
+    double orta = (a + b + c) / 3;
+    cout << "Ededi orta: " << orta << endl;
 }
 
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
 
-
-int main(){
-	double a,b,c;
-	cout<<"ededleri daxil edin:"<<endl;
-	
-	cin>>a;
-	cin>>b;
-	cin>>c;
-	
-	cout<<"ededi orta:"<<edediOrta(a,b,c)<<endl;
-
+    edediOrta(a, b, c);
 }
-	
-	
-	
-	
-
-
