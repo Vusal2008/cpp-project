751ba25fed872707d9f8fa43db171b5fa1a54f74
Vusal2008 <velekberov80@gmail.com>
2025-11-30 00:45:59 +0400

    level 5

--- task6.cpp
+++ task6.cpp
@@ -1,48 +1,20 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-//3 edetden boyuyunu tapan fuction//
 
-
-void comparison2(int a,int b, int c){
-	
-	if (a>b,a>c)
-	{
-	cout<<a;
-	}else if(a>b,a>c)
-	{
-		cout<<a;
-	}else if(a<b,a>c)
-	{
-		cout<<b;
-	}else if(b>a,b<c)
-	{
-		cout<<c;
-	}else if(a==c)
-	{
-	cout<<c;
-	}
-	else if(a==b)
-	{
-	cout<<b;
-	}
-	else if(b==c){cout<<c;
-	}
-	
-	
+void findMax(int a, int b, int c)
+{
+    if (a >= b && a >= c)
+        cout << a;
+    else if (b >= a && b >= c)
+        cout << b;
+    else
+        cout << c;
 }
 
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
 
-int main(){
-	
-	int a;
-	cout<<"1 ci ededi daxil edin:";
-	cin>>a;
-	int b;
-	cout<<"2 ci ededi daxil edin:";
-	cin>>b;
-	int c;
-	cout<<"3 cu ededi daxil edin:";
-	cin>>c;
-	
-	comparison2(a,b,c);
+    findMax(a, b, c);
 }
