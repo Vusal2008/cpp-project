00e1baf39537f6ba86258f927332cb327d6bcd2a
Vusal2008 <velekberov80@gmail.com>
2025-11-30 00:41:54 +0400

    level 5

--- task3.cpp
+++ task3.cpp
@@ -1,28 +1,19 @@
-# include <iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-void  comparison(int a,int b){
-	
-	if(a>b){
-		cout<<"boyuk olan eded:"<<a;
-	}
-	else if (a==b){
-		cout<<"ededler beraberdir"<<endl;
-	}
-	else if (a<b){
-		
-		cout<<"boyuk olan eded:"<<b;
-	}
+void comparison(int a, int b)
+{
+    if (a > b)
+        cout << "boyuk olan eded: " << a;
+    else if (a < b)
+        cout << "boyuk olan eded: " << b;
+    else
+        cout << "ededler beraberdir";
 }
 
-int main(){
-int a;
-cout<<"1-ci ededi daxil edin";
-cin>>a;
-int b;	
-cout<<"2-ci ededi daxil edin";
-cin>>b;
-	
-	comparison(a,b);
-	
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    comparison(a, b);
 }
