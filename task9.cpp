78415f2fbac20bb7640c17d93461ad4c75c831be
Vusal2008 <velekberov80@gmail.com>
2025-11-30 00:50:24 +0400

    level 5

--- task9.cpp
+++ task9.cpp
@@ -1,20 +1,18 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-//daxil edilmis edede qeder butun reqemlerin cemini tapan function//
-int eded(int a){
-	int b=0;
-    
- for(int i = 1;i<=a;i++){
- 	b=i+b;	
- }
- cout<<"cem:"<<b<<endl;
+
+void eded(int a)
+{
+    int b = 0;
+    for (int i = 1; i <= a; i++)
+        b += i;
+
+    cout << "cem: " << b << endl;
 }
 
-int main(){
-	
-	int a;
-	cout<<"ededi daxil edin:";
-	cin>>a;
-	eded(a); 
-	
+int main()
+{
+    int a;
+    cin >> a;
+    eded(a);
 }
