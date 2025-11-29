8eb924d67ca31a0be3aa271add8744a0b1940242
Vusal2008 <velekberov80@gmail.com>
2025-11-30 00:46:41 +0400

    level 5

--- task7.cpp
+++ task7.cpp
@@ -1,29 +1,18 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-//daxil edilmis ededin bolenlerini tapan fuction//
-int bolenler(int a){
 
-	for (int i=1;i<=a;i++){
-		
-		if (a%i==0){
-			cout<<i<<","<<endl;
-		}		
-		else{cout<<"";
-		}
-	}
-	
-	
+void bolenler(int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        if (a % i == 0)
+            cout << i << endl;
+    }
 }
 
-
-
-
-
-int main(){
-	int a;
-	cout<<"ededi daxil edin:";
-	cin>>a;
-	 bolenler(a);
-	
-	
+int main()
+{
+    int a;
+    cin >> a;
+    bolenler(a);
 }
